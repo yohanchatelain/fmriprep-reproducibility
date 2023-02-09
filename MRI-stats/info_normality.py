642e6a64aae153e8f80b874b0d2ed65a6757985b
Yohan Chatelain <yohan.chatelain@gmail.com>
2023-02-09 15:27:21 -0500

    Add plot

--- MRI-stats/info_normality.py
+++ MRI-stats/info_normality.py
@@ -1,4 +1,7 @@
 
+import plotly.graph_objects as go
+import numpy as np
+import plotly.express as px
 import pandas as pd
 import sys
 import glob
@@ -37,14 +40,60 @@ def get_log(directory):
     for file in files:
         (dataset, subject, fwh, alpha) = get_params(file)
         ratio = parse_file(file)
-        df.loc[-1] = [dataset, subject, fwh, alpha, ratio]
+        df.loc[-1] = [dataset, subject, float(fwh), float(alpha), float(ratio)]
         df.index += 1
 
     df.sort_index(inplace=True)
     return df
 
 
+def plot_facet(df):
+    print('Plot facet')
+    fig = px.scatter(df, x='alpha', y='ratio', color='fwh',
+                     color_continuous_scale='Jet', facet_col='subject')
+
+    fig.update_layout(
+        title='% of voxels rejecting Shapiro-Wilk normality test')
+    fig.update_yaxes(range=[-1, 101])
+    fig.write_image('facet.png', scale=2)
+    fig.write_html('facet.html')
+
+
+def plot_subject(df):
+    subject = df['subject'].unique()[0]
+    print(f'Plot {subject}')
+    fig = px.scatter(df, x='alpha', y='ratio',
+                     color='fwh', color_continuous_scale='Jet')
+    nb_fwh = df['fwh'].unique().size
+    nb_alpha = df['alpha'].unique().size
+
+    alpha = df['alpha'].unique()
+    alpha.sort()
+    alpha_line = go.Scatter(x=alpha, y=100 * alpha, mode='lines', line=dict(color='black'),
+                            name='nominal value')
+
+    shift = np.linspace(0.99, 1.11, nb_fwh)
+    fig.data[0]['x'] *= np.repeat(shift, nb_alpha)
+    fig.update_yaxes(range=[-1, 101])
+
+    fig.add_trace(alpha_line)
+
+    fig.update_layout(
+        title=f'% of voxels rejecting Shapiro-Wilk normality test ({subject})')
+    fig.write_image(f'{subject}.png', scale=2)
+    fig.write_html(f'{subject}.html')
+
+
+def plot(df):
+    df.sort_values(by=['dataset', 'subject', 'fwh', 'alpha'], inplace=True)
+    plot_facet(df)
+    subjects = df['subject'].unique()
+    for subject in subjects:
+        plot_subject(df[df['subject'] == subject])
+
+
 if __name__ == '__main__':
     directory = sys.argv[1]
     d = get_log(directory)
-    print(d)
+    d.to_csv('non-normal-log.csv')
+    plot(d)
