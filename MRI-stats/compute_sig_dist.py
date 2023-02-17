3ad88cf73bf04805c04ad2a49fda1a2fcf39a851
yohanchatelain <yohan.chatelain@gmail.com>
2023-02-17 14:43:29 -0500

    Compute sig diff

---
+++ MRI-stats/compute_sig_dist.py
@@ -0,0 +1,115 @@
+import argparse
+import os
+import mri_image
+from sklearn.model_selection import KFold
+import significantdigits
+import numpy as np
+import pickle
+
+
+def compute_sig(array, reference):
+    sig = significantdigits.significant_digits(array,
+                                               reference=reference,
+                                               method=significantdigits.Method.General)
+    return sig
+
+
+def compute_k_fold_sig(args, reference_T1, reference_mask):
+    mask_comb = args.mask_combination
+    fwh = args.smooth_kernel
+
+    kfold = KFold(len(reference_T1))
+
+    global_reference = np.mean(reference_T1, axis=0)
+    global_sig = compute_sig(reference_T1, global_reference)
+
+    hists = []
+
+    for i, _ in kfold.split(reference_T1):
+        t1_test = reference_T1[i]
+        mask_test = reference_mask[i]
+
+        t1_masked, supermask = mri_image.mask_t1(
+            t1_test, mask_test, mask_comb, fwh)
+
+        reference = np.mean(t1_masked, axis=0)
+        sig = compute_sig(t1_masked, reference)
+
+        diff = np.abs(global_sig - sig)
+        hist = np.histogram(diff, nbins=range(13))
+        hists.append(hist)
+
+    return hists
+
+
+def get_filename(args):
+    prefix = args.reference_prefix.replace(os.path.sep, '-')
+    return '_'.join(map(str, [prefix,
+                              args.reference_dataset,
+                              args.reference_subject,
+                              args.template,
+                              args.fwh]))
+
+
+def compute_sig_loo(args):
+    '''
+    compute significant digits for leave-one-out
+    '''
+    reference_t1s, reference_masks = mri_image.get_reference(
+        prefix=args.reference_prefix,
+        subject=args.reference_subject,
+        dataset=args.reference_dataset,
+        template=args.template,
+        data_type=args.data_type)
+
+    reference_sample_size = len(reference_t1s)
+
+    print(f'Sample size: {reference_sample_size}')
+
+    hists = compute_k_fold_sig(args, reference_t1s, reference_masks)
+
+    filename = get_filename(args)
+
+    with open(filename, 'wb') as fo:
+        pickle.dump(hists, fo)
+
+
+default_templates = ['MNI152NLin2009cAsym', 'MNI152NLin6Asym']
+
+
+def init_global_args(parser):
+    parser.add_argument("--template", action="store",
+                        choices=default_templates,
+                        required=True, help="Template")
+    parser.add_argument("--data-type", action="store",
+                        default='anat', choices=['anat'],
+                        required=True, help="Data type")
+    parser.add_argument("--reference-prefix", action='store',
+                        required=True, help='Reference prefix path')
+    parser.add_argument("--reference-dataset", action="store",
+                        required=True, help="Dataset reference")
+    parser.add_argument("--reference-subject", action="store",
+                        required=True, help="Subject reference")
+    parser.add_argument('--smooth-kernel', action='store',
+                        type=float, default=0.0,
+                        help='Size of the kernel smoothing')
+    parser.add_argument('--mask-combination', action='store', type=str,
+                        choices=['union', 'intersection', 'map'],
+                        default='union',
+                        help='Method to combine brain mask (map applies each brain mask to the image repetition)')
+
+
+def parse_args():
+    parser = argparse.ArgumentParser('sig diff')
+    init_global_args(parser)
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    compute_sig_loo(args)
+
+
+if __name__ == '__main__':
+    main()
