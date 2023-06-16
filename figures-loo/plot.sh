884e8621a66a80f6b4c546502354d3aace5e5f94
yohanchatelain <yohan.chatelain@gmail.com>
2023-06-16 11:14:11 -0400

    add plot loo

--- figures-loo/plot.sh
+++ figures-loo/plot.sh
@@ -1,3 +1,3 @@
-python3 ../MRI-parse/parse_mri.py --reference ../pickle/exclude/union/rr ../pickle/exclude/union/rs --test exclude --mct-method fwe_bonferroni 
+python3 ../MRI-parse/parse_mri.py --reference ../pickle/exclude/union/rr ../pickle/exclude/union/rs --test exclude --mct-method fwe_bonferroni
 mv exclude_mct_fwe_bonferroni.pdf loo_fwe_bonferroni.pdf
 mv exclude_pce.pdf loo_pce.pdf
